0314e50bd925895c95c86a648f2325f4337b452f
김한철 <05.hanch.it@gmail.com>
2021-07-13 17:22:19 +0900

    feat : 공통으로 사용될 header 추가
    로그인, 회원가입, 카테고리, 메뉴

---
+++ client/src/pages/components/commonHeader.ts
@@ -0,0 +1,13 @@
+import { createElem, HTMLElementAttrs } from '@/src/utils/createElem';
+import { svgIcons } from '@/src/pages/components/svgIcons';
+
+export default function (attrs: HTMLElementAttrs, title: string) {
+  const _leftArrow = createElem('button', { class: 'header-left-arrow svg-icon' } as HTMLElementAttrs);
+  _leftArrow.innerHTML = svgIcons.chevronLeft;
+
+  const _headerTitle = createElem('div', { class: 'header-title' } as HTMLElementAttrs);
+  _headerTitle.innerText = title;
+
+  attrs.class = attrs.class + ' common-header';
+  return createElem('header', { ...attrs }, _leftArrow, _headerTitle);
+}
