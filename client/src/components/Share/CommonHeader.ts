a116ea46bc221e66d2ddcb5916d51e832bbb630d
eunseonglee <eunseongofficial@gmail.com>
2021-07-14 20:52:54 +0900

    fix: 충돌 수정

--- client/src/components/Share/CommonHeader.ts
+++ client/src/components/Share/CommonHeader.ts
@@ -1,15 +1,8 @@
-import Component, { addEvent } from '@/src/interfaces/Component';
+import Component from '@/src/interfaces/Component';
 import { svgIcons } from '@/src/assets/svgIcons';
 
-const CommonHeader = function (this: Component, $target: HTMLElement, props: any) {
-  const setup = () => {
-    this.$target = $target || null;
-    this.$props = props;
-    this.render();
-    this.setEvent();
-  };
-
-  this.template = () => {
+class CommonHeader extends Component {
+  template() {
     return `
         <nav class="common-header">
           <ul>
@@ -18,29 +11,11 @@ const CommonHeader = function (this: Component, $target: HTMLElement, props: any
           </ul>
         </nav>
       `;
-  };
-
-  this.render = () => {
-    this.$target.innerHTML = this.template();
-    this.mounted();
-  };
-
-  this.mounted = () => {};
-
-  this.setState = (nextState: object) => {
-    this.$state = { ...this.$state, ...nextState };
-    this.render();
-  };
-
-  this.setEvent = () => {
+  }
+  setEvent() {
     const { leftArrowEvent } = this.$props;
-    console.log(this.$props);
     this.addEvent('click', '[data-btn="left-arrow"]', leftArrowEvent);
-  };
-
-  this.addEvent = addEvent;
-
-  setup();
-} as any;
+  }
+}
 
 export default CommonHeader;
