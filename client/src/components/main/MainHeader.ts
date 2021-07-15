8dd9e2a7429f0d86503feb63a04aa248649c9a86
eunseonglee <eunseongofficial@gmail.com>
2021-07-16 01:59:47 +0900

    [#37] feat: 헤더의 지역 버튼 클릭 시 dropdown 모달 생성

--- client/src/components/main/MainHeader.ts
+++ client/src/components/main/MainHeader.ts
@@ -1,5 +1,6 @@
 import Component from '@/src/interfaces/Component';
 import { svgIcons } from '@/src/assets/svgIcons';
+import DropDown from '../Share/DropDown';
 
 export default class MainHeader extends Component {
   template() {
@@ -7,7 +8,7 @@ export default class MainHeader extends Component {
         <nav>
           <ul>
             <li data-btn='catagory-toggle'>${svgIcons.categoty}</li>
-            <li>${svgIcons.mapPin}<div class='type-link medium'>&nbsp;${this.$props.title}</div></li>
+            <li data-btn="user-set-location">${svgIcons.mapPin}<div class='type-link medium'>&nbsp;${this.$props.title}</div></li>
             <li>
               <a class="userBtn" href="#/signin">${svgIcons.user}</a>
               <a class="menuBtn" data-btn='menu-toggle' >${svgIcons.menu}</a>
@@ -16,9 +17,21 @@ export default class MainHeader extends Component {
         </nav>
       `;
   }
+
   setEvent() {
-    const { toggleCategory, toggleMenu } = this.$props;
+    const { toggleCategory, toggleMenu, onClickItem, onClickSettingLocation } = this.$props;
     this.addEvent('click', '[data-btn="catagory-toggle"]', toggleCategory);
     this.addEvent('click', '[data-btn="menu-toggle"]', toggleMenu);
+    this.addEvent('click', '[data-btn="user-set-location"]', (e: any) => {
+      const $dropdownTarget = e.target.closest('.main-header');
+      const $dropDownDiv = document.createElement('div');
+      $dropDownDiv.className = 'dropdown-container';
+      $dropdownTarget.appendChild($dropDownDiv);
+      new DropDown($dropDownDiv, {
+        labels: ['역삼동'],
+        onClickItem,
+        specialItems: [{ name: '내 지역 설정하기', type: 'setting-location', onClickSettingLocation }],
+      });
+    });
   }
 }
