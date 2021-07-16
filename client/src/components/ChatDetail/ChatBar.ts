e2534427a4fbf7bcdd43bbdf748ee2379116ec1d
eunseonglee <eunseongofficial@gmail.com>
2021-07-17 02:11:28 +0900

    [#43] feat: sendButton 컴포넌트 분리

--- client/src/components/ChatDetail/ChatBar.ts
+++ client/src/components/ChatDetail/ChatBar.ts
@@ -1,20 +1,29 @@
 import Component from '@/src/interfaces/Component';
-import { svgIcons } from '@/src/assets/svgIcons';
-
-interface ChatBarProps {}
+import ChatSendButton from './ChatSendButton';
 
 export default class ChatBar extends Component {
+  setup() {
+    this.$state = {
+      isSendButtonActive: false,
+    };
+  }
+
   template() {
     return `
-    <div class="chat-inner">
+    <div class="chat-inner" deactive>
       <textarea id="chatBar-input" rows="1" placeholder="메세지를 입력하세요"></textarea>
     </div>
-      ${svgIcons.send}
+      <div class="send-svg"></div>
     `;
   }
 
+  mounted() {
+    const $sendBtn = this.$target.querySelector('.send-svg') as HTMLElement;
+    new ChatSendButton($sendBtn, {});
+  }
+
   setEvent() {
-    this.addEvent('keydown', '#chatBar-input', (e: any) => {
+    this.addEvent('input', '#chatBar-input', (e: any) => {
       const $chatBar = this.$target.querySelector('#chatBar-input') as HTMLElement;
       if (e.target.scrollHeight <= 70) {
         $chatBar.style.height = 'auto';
