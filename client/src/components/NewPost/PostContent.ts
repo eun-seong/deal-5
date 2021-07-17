44f73a723f91027b5bcbd7394293aae7fc634259
eunseonglee <eunseongofficial@gmail.com>
2021-07-18 02:12:23 +0900

    fix: autoHeightTextarea로 코드 변경

--- client/src/components/NewPost/PostContent.ts
+++ client/src/components/NewPost/PostContent.ts
@@ -1,4 +1,5 @@
 import Component from '@/src/interfaces/Component';
+import autoHeightTextarea from '@/src/assets/utils/autoHeightTextarea';
 
 export default class PostContent extends Component {
   template() {
@@ -9,10 +10,7 @@ export default class PostContent extends Component {
 
   setEvent() {
     this.addEvent('input', '#post-content', (e: any) => {
-      console.log('hi');
-      const $chatBar = this.$target.querySelector('#post-content') as HTMLElement;
-      $chatBar.style.height = 'auto';
-      $chatBar.style.height = e.target.scrollHeight + 'px';
+      autoHeightTextarea(e, this.$target, '#post-content');
     });
   }
 }
