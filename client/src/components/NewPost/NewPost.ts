afc9f448f9aeb0bd0e132458a50a7157f482b1e5
eunseonglee <eunseongofficial@gmail.com>
2021-07-17 19:09:34 +0900

    [#45] feat: NewPost 기본틀 세팅

---
+++ client/src/components/NewPost/NewPost.ts
@@ -0,0 +1,52 @@
+import Component from '@/src/interfaces/Component';
+import CommonHeader from '../Share/CommonHeader';
+import { svgIcons } from '@/src/assets/svgIcons';
+import historyBack from '@/src/assets/utils/historyBack';
+import ImagesHolder from './ImagesHolder';
+import PostTitle from './PostTitle';
+import ItemPrice from './ItemPrice';
+import PostConent from './PostContent';
+import SaleLocation from './SaleLocation';
+
+export default class NewPost extends Component {
+  template() {
+    return `
+    <header data-component="header"></header>
+    <div class="content">
+      <div data-component="images-holder"></div>
+      <div data-component="post-title"></div>
+      <div data-component="item-price"></div>
+      <div data-component="post-content"></div>
+      <div data-component="sale-location"></div>
+    </div>
+    `;
+  }
+
+  mounted() {
+    const $header = this.$target.querySelector('[data-component="header"]');
+    const $imagesHolder = this.$target.querySelector('[data-component="images-holder"]');
+    const $postTitle = this.$target.querySelector('[data-component="post-title"]');
+    const $itemPrice = this.$target.querySelector('[data-component="item-price"]');
+    const $postContent = this.$target.querySelector('[data-component="post-content"]');
+    const $saleLocation = this.$target.querySelector('[data-component="sale-location"]');
+
+    new CommonHeader($header as HTMLElement, {
+      leftArrowEvent: historyBack,
+      title: '글쓰기',
+      rightButton: {
+        svg: svgIcons.check,
+        colorType: 'gray1',
+        handleClick: this.handlePostingButton.bind(this),
+      },
+    });
+    new ImagesHolder($imagesHolder as HTMLElement);
+    new PostTitle($postTitle as HTMLElement);
+    new ItemPrice($itemPrice as HTMLElement);
+    new PostConent($postContent as HTMLElement);
+    new SaleLocation($saleLocation as HTMLElement);
+  }
+
+  handlePostingButton() {
+    console.log('post');
+  }
+}
