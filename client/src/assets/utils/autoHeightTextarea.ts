e7ef6e0ca15357655d8e1dbe593317ad1635bc97
eunseonglee <eunseongofficial@gmail.com>
2021-07-18 23:58:12 +0900

    [#43] refactor: event 파라미터가 불필요하여 삭제

--- client/src/assets/utils/autoHeightTextarea.ts
+++ client/src/assets/utils/autoHeightTextarea.ts
@@ -1,7 +1,7 @@
-export default (event: any, target: HTMLElement, selector: string) => {
+export default (target: HTMLElement, selector: string) => {
   const $textarea = target.querySelector(selector) as HTMLElement;
-  if (event.target.scrollHeight <= 70) {
+  if ($textarea.scrollHeight <= 70) {
     $textarea.style.height = 'auto';
-    $textarea.style.height = event.target.scrollHeight + 'px';
+    $textarea.style.height = $textarea.scrollHeight + 'px';
   }
 };
