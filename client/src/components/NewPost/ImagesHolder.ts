72b630ea451e7698e570e7d0f774ead9e381b6c7
eunseonglee <eunseongofficial@gmail.com>
2021-07-18 21:14:13 +0900

    [#48] refactor: 함수 객체를 클래스 함수로 변경

--- client/src/components/NewPost/ImagesHolder.ts
+++ client/src/components/NewPost/ImagesHolder.ts
@@ -20,60 +20,61 @@ export default class ImagesHolder extends Component {
   }
 
   mounted() {
-    const readFile = (file: File) => {
-      const reader = new FileReader();
-      reader.addEventListener('load', (e: any) => {
-        // 사진 추가
-        const lastID = this.images.length === 0 ? 0 : this.images[this.images.length - 1].id + 1;
-        this.images = [...this.images, { id: lastID, path: e.target.result }];
-        const $li = document.createElement('li');
-        $li.setAttribute('img-id', String(this.images.length));
-        new ImageButton($li, { img_src: e.target.result, removeImage: removeImage });
-        $images?.appendChild($li);
-
-        // 사진 개수 update
-        this.updateNumOfImages();
-      });
-      reader.readAsDataURL(file);
-    };
-
-    const selectImage = () => {
-      if (this.images.length > 10) {
-        // TODO Alert 처리
-        return;
-      }
-
-      const $imageInput = this.$target.querySelector('#img-select') as HTMLInputElement;
-      const selectedFiles = $imageInput.files;
-
-      if (selectedFiles) [].forEach.call(selectedFiles, readFile);
-    };
-
-    const removeImage = (e: any) => {
-      // 사진 삭제
-      const imgId = parseInt(e.target.closest('li').getAttribute('img-id'));
-      this.images = this.images.filter(img => img.id !== imgId);
-      e.target.closest('li').remove();
-
-      // 사진 개수 update
-      this.updateNumOfImages();
-    };
-
     const $images = this.$target.querySelector('[data-component="images"]');
     /* 사진 추가 버튼 */
     const $li = document.createElement('li');
     new ImageAddButton($li, {
       imageNum: this.images.length,
-      selectedImage: selectImage,
+      selectedImage: this.selectImage.bind(this),
     });
     $images?.appendChild($li);
 
     this.images.forEach((img: Image, i: number) => {
       const $li = document.createElement('li');
       $li.setAttribute('img-id', String(i));
-      new ImageButton($li, { img_src: img, removeImage: removeImage });
+      new ImageButton($li, { img_src: img, removeImage: this.removeImage.bind(this) });
       $images?.appendChild($li);
     });
+  }
+
+  readFile = (file: File) => {
+    const $images = this.$target.querySelector('[data-component="images"]');
+    const reader = new FileReader();
+    reader.addEventListener('load', (e: any) => {
+      // 사진 추가
+      const lastID = this.images.length === 0 ? 0 : this.images[this.images.length - 1].id + 1;
+      this.images = [...this.images, { id: lastID, path: e.target.result }];
+      const $li = document.createElement('li');
+      $li.setAttribute('img-id', String(this.images.length));
+      new ImageButton($li, { img_src: e.target.result, removeImage: this.removeImage.bind(this) });
+      $images?.appendChild($li);
+
+      // 사진 개수 update
+      this.updateNumOfImages();
+    });
+    reader.readAsDataURL(file);
+  };
+
+  selectImage() {
+    if (this.images.length > 10) {
+      // TODO Alert 처리
+      return;
+    }
+
+    const $imageInput = this.$target.querySelector('#img-select') as HTMLInputElement;
+    const selectedFiles = $imageInput.files;
+
+    if (selectedFiles) [].forEach.call(selectedFiles, this.readFile);
+  }
+
+  removeImage(e: any) {
+    // 사진 삭제
+    const imgId = parseInt(e.target.closest('li').getAttribute('img-id'));
+    this.images = this.images.filter(img => img.id !== imgId);
+    e.target.closest('li').remove();
+
+    // 사진 개수 update
+    this.updateNumOfImages();
   }
 
   updateNumOfImages() {
