3996e5d004a51a211c0c09a0dddd7df9fe15b26e
김한철 <05.hanch.it@gmail.com>
2021-07-19 11:02:46 +0900

    chore : 서버 셋팅

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import path from 'path';
-import routers from './routers';
+// import routers from './routers';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 81;
 
 const app: express.Application = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src')));
-app.use(routers);
+// app.use(routers);
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('start');
@@ -17,3 +17,6 @@ app.get('/', (req: express.Request, res: express.Response) => {
 app.listen(port, () => {
   console.log(`여기 -> http://localhost:${port}`);
 });
+
+
+
