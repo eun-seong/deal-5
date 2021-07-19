a347e42639a5684cd4104a4abebac29101dc44d9
eunseonglee <eunseongofficial@gmail.com>
2021-07-20 00:07:21 +0900

    [#56] feat: jwt 토큰 발급
    로그인할 때 jwt 토큰을 발급합니다.

--- server/action/user.ts
+++ server/action/user.ts
@@ -1,17 +1,34 @@
 import { Request, Response } from 'express';
 import { execQuery, selectQuery } from '../db';
+import { sign } from './auth';
 import USER_QUERY from '../query/user';
 
 const actionLogin = async (req: Request, res: Response) => {
   try {
     const { user_id, pw } = req.body;
     const result = await selectQuery(USER_QUERY.queryLogin({ user_id, pw }));
-    const data = JSON.parse(result)[0];
-    data.code = 1;
-    data.message = '성공적으로 로그인 되었습니다.';
-    res.send(JSON.stringify(data));
+    const data: {
+      id: number;
+      user_id: string;
+      nickname: string;
+      location_1: string;
+    } = JSON.parse(result)[0];
+
+    const accessToken = sign(data.id, data.nickname);
+    res.status(200).send({
+      ok: true,
+      code: 1,
+      message: '성공적으로 로그인 되었습니다.',
+      accessToken,
+      data: {
+        u_id: data.id,
+        user_id: data.user_id,
+        nickname: data.nickname,
+        location: data.location_1,
+      },
+    });
   } catch (err) {
-    res.send(JSON.stringify({ messagke: '로그인에 실패하였습니다.', code: 2 }));
+    res.status(401).send({ ok: false, message: '로그인에 실패하였습니다.' });
   }
 };
 
