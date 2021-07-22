af3483895484092ed4e68edc67c7304a3d318bd1
eunseonglee <eunseongofficial@gmail.com>
2021-07-22 15:28:15 +0900

    [#95] feat: 글쓰기에서 내 지역 표시

--- client/src/components/NewPost/SaleLocation.ts
+++ client/src/components/NewPost/SaleLocation.ts
@@ -1,13 +1,28 @@
 import Component from '@/src/interfaces/Component';
 import { svgIcons } from '@/src/assets/svgIcons';
+import { api_getLocation } from '@/src/apis/user';
 
 export default class SaleLocation extends Component {
+  setup() {
+    this.$state = {
+      location: '',
+    };
+    api_getLocation({})
+      .then((res: any) => {
+        const { location_1 } = res.data;
+        this.setState({
+          location: location_1,
+        });
+      })
+      .catch(e => console.log(e));
+  }
+
   template() {
-    const { location } = this.$props;
+    const { location } = this.$state;
 
     return `
     <span>${svgIcons.mapPin}</span>
-    <span>${location || '역삼동'}</span>
+    <span>${location || ''}</span>
     `;
   }
 
