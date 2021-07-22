7db1c1c1cfc93dfedb74a053aa7216f248c84f8a
eunseonglee <eunseongofficial@gmail.com>
2021-07-23 01:14:39 +0900

    [#92] feat: 글쓰기 수정하기 기능 완료

--- server/routers/newpost.ts
+++ server/routers/newpost.ts
@@ -43,30 +43,19 @@ router.post('/upload-img', authJWT, upload.array('img', 10), (req: any, res, nex
 
 router.post('/upload', authJWT, async (req: any, res) => {
   try {
-    console.log(req.user);
     if (req.user) {
-      console.log(req.body);
-      const { category, title, discription, price, img_list, deletedExistingImages } = req.body;
-      console.log(img_list);
-      deletedExistingImages((img: string) => {
-        const delIndex = img_list.indexOf(img);
-        if (delIndex !== -1) {
-          console.log('del', img_list[delIndex]);
-          img_list.splice(delIndex, 1);
-        }
-      });
-      console.log(img_list);
-      // const data = await execQuery(
-      //   NEWPOST_QUERY.queryInsertItem({
-      //     user_id: req.user.id,
-      //     category: category,
-      //     title: title,
-      //     discription: discription,
-      //     price: price,
-      //     img_list: JSON.stringify(img_list),
-      //   })
-      // );
-      return res.send({ ok: true, message: '글쓰기 완료!' });
+      const { category, title, discription, price, img_list } = req.body;
+      const data = await execQuery(
+        NEWPOST_QUERY.queryInsertItem({
+          user_id: req.user.id,
+          category: category,
+          title: title,
+          discription: discription,
+          price: price,
+          img_list: JSON.stringify(img_list),
+        })
+      );
+      return res.send({ ok: true, message: '글쓰기 완료!', id: data.insertId });
     } else {
       return res.send({ ok: false, message: '사용자 인증 실패' });
     }
@@ -75,11 +64,45 @@ router.post('/upload', authJWT, async (req: any, res) => {
   }
 });
 
+router.post('/edit', authJWT, async (req: any, res) => {
+  console.log(req.body);
+  try {
+    if (req.user) {
+      const { id, category, title, discription, price, img_list, deletedList } = req.body;
+
+      // 글 수정하기에서 서버에 있던 사진을 삭제할 경우 파일 제거
+      if (deletedList.length !== 0) {
+        deletedList.forEach((imgPath: string) => {
+          const delIndex = img_list.indexOf(imgPath);
+          img_list.splice(delIndex, 1);
+          fs.unlink(imgPath, err => {
+            console.log(err);
+          });
+        });
+      }
+
+      const data = await execQuery(
+        NEWPOST_QUERY.queryUpdateItem({
+          id: id,
+          category: category,
+          title: title,
+          discription: discription,
+          price: price,
+          img_list: JSON.stringify(img_list),
+        })
+      );
+      return res.send({ ok: true, message: '포스트 수정 완료!', id: id });
+    } else {
+      return res.send({ ok: false, message: '사용자 인증 실패' });
+    }
+  } catch (err) {
+    return res.send({ ok: false, message: '포스트 수정 실패' });
+  }
+});
+
 router.post('/get-post', authJWT, async (req: any, res) => {
   try {
-    console.log(req.user);
     if (req.user) {
-      console.log(req.body);
       const { id } = req.body;
       const result = await selectQuery(NEWPOST_QUERY.queryGetPostContents(id));
       const data: {
